579945591994de8f5c2ee9a59285ee9786634fa1
CJOcode <cjoconnor24@gmail.com>
2022-05-21 13:57:23 -0700

    half way done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,9 +14,9 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
 
@@ -28,30 +28,98 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const num1 = 15
-// Expected output: "15 is divisible by three"
-const num2 = 0
-// Expected output: "0 is divisible by three"
-const num3 = -7
-// Expected output: "-7 is not divisible by three"
+// const num1 = 15
+// // Expected output: "15 is divisible by three"
+// const num2 = 0
+// // Expected output: "0 is divisible by three"
+// const num3 = -7
+// // Expected output: "-7 is not divisible by three"
 
+//a describe method that lists the name of a function called divisibleByThree test.
+// describe("divisibleByThree", () => {
+//     // a test/it method, nested within the describe block, that in plain words, describes what that the function does.
+//     it("returns a string that tells you if the number is divisible by three", () => {
+//         const num1 = 15
+// // Expected output: "15 is divisible by three"
+//         const num2 = 0
+// // Expected output: "0 is divisible by three"
+//         const num3 = -7
+// // Expected output: "-7 is not divisible by three"
+//         //an expect method, nested within the test block, calling on the divisibleByThree function, followed by the .toEqual() matcher that checks the expected output of the function return.
+//         expect(divisibleByThree(15)).toEqual("is divisible by three")
+//         expect(divisibleByThree(0)).toEqual("is divisible by three")
+//         expect(divisibleByThree(-7)).toEqual("is not divisible by three")
+//     })
+// })
+// test had a GOOD failure.
+// FAIL  ./code-challenges.test.js
+// divisibleByThree
+//   ✕ returns a string that tells you if the number is divisible by three (1 ms)
+
+// ● divisibleByThree › returns a string that tells you if the number is divisible by three
+
+//   ReferenceError: divisibleByThree is not defined
 
 // b) Create the function that makes the test pass.
 
+// Create a function called divisibleByThree
+
+// Pseudo
+// create a function named divisibleByThree 
+    // the function is going to decide whether or not the number is divisible by three
+    // the function will have a conditional if/else statement using, num % 3 === 0
+    // return a string stating "is divisible by three" if true, and "is not divisible by three" if false.
+
+// const divisibleByThree = (num) => {
+//     if (num % 3 === 0) {
+//         return "is divisible by three"
+//     } else {
+//         return "is not divisible by three"
+//     }
+// }
+//     divisibleByThree()
+
+    // Code passed.
+    // PASS  ./code-challenges.test.js
+    // divisibleByThree
+    //   ✓ returns a string that tells you if the number is divisible by three (2 ms)
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+//a describe method that lists the name of a function called capitalizeWords test.
+// describe("capitalizeWords", () => {
+//     // a test/it method, nested within the describe block, that in plain words, describes what that the function does.
+//     it("returns an array with all words capitalized", () => {
 
+//         const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+//         // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+//         const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+//         // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+
+//         //an expect method, nested within the test block, calling on the divisibleByThree function, followed by the .toEqual() matcher that checks the expected output of the function return.
+//         expect(capitalizeWords(randomNouns1)).toEqual("STREETLAMP", "POTATO", "TEETH", "CONCLUSION", "NEPHEW")
+//         expect(capitalizeWords(randomNouns2)).toEqual("TEMPERATURE", "DATABASE", "CHOPSTICKS", "MANGO", "DEDUCTION")
+//     })
+// })
+
+// test had a GOOD failure
+// FAIL  ./code-challenges.test.js
+// capitalizeWords
+//   ✕ returns an array with all words capitalized (1 ms)
+
+// ● capitalizeWords › returns an array with all words capitalized
+
+//   ReferenceError: capitalizeWords is not defined
 
 // b) Create the function that makes the test pass.
-
+const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+const capitalizeWords = (array) => {
+    return array.map(string => string.toUpperCase)
+}
+console.log(capitalizeWords(randomNouns2))
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
