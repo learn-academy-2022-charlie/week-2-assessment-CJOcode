9383c2aacc3f61ef8c7440b56529db9e25100bb0
CJOcode <cjoconnor24@gmail.com>
2022-05-21 19:03:46 -0700

    Working on question 4

--- coding-concepts.js
+++ coding-concepts.js
@@ -9,9 +9,14 @@
 
 var cohort = "Bravo 2022"
 // console.log(cohort.split(""))
+// ---> [
+//   'B', 'r', 'a', 'v',
+//   'o', ' ', '2', '0',
+//   '2', '2'
+// ]
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: the output will be the string inside of a array due to the .split method
+// b) Verify and explain: I was partially correct because the string was placed into an array but also each letter was separated into its own index. If the .split method did not contain the quotes inside of parenthesis it would have not split the letters up into separate index's.
 
 
 // --------------------2) What will this log?
@@ -21,8 +26,10 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 
+  // This will log "Hello, LEARN student" because the console.log calls on the greeter method which places the string "LEARN student" into the parameter where it gets placed into the ${name}.
+// b) Verify and explain: 
+  // I was wrong! I am frustrated with my self because I immediately knew what the problem was when I saw the output as undefined. It was simply missing the return statement inside of the function before the `Hello, ${name}!`. If it did have the return it would have had an output of what I stated above. The ${} is called string interpolation which places the string in side of the curly braces instead of having to use + to add the strings together.
 
 
 // --------------------3) What will this log?
@@ -30,18 +37,21 @@ const greeter = (name) => {
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 // console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 
+  // Each number inside of the array should multiply by 2. The .map method iterates through each index and its value returning an array of the same length allow for the numbers to be multiplied.
+// b) Verify and explain: 
+  // I was correct the output was each number multiplied by 2. As I said above the .map high order function iterates through the array and multiplies each number. Using the arrow function allows us to reduce the code to less lines while the .map allows us to not have to write out a for loop.
 
 
 // --------------------4) What will this log?
 
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
-
+// a) Your answer: 
+  // This should log only th odd number in a new array  because its function is stating to log only the number that do not have a remainder of 0 when using the modulo of 2. The filter method takes the array and logs a new one of the the elements that are truthy to the condition.
+// b) Verify and explain: 
+  
 
 // --------------------5) What will this log?
 
